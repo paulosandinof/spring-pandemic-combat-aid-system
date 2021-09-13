110a0d7ec69b63992eec1a81b334d11373fa8483
Paulo Sandino <paulosandino@gmail.com>
2021-09-13 16:46:57 -0300

    first commit

---
+++ src/main/java/com/sandino/pandemiccombataidsystem/PandemicCombatAidSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sandino.pandemiccombataidsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemicCombatAidSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandemicCombatAidSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandino/pandemiccombataidsystem/PandemicCombatAidSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandino.pandemiccombataidsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicCombatAidSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
