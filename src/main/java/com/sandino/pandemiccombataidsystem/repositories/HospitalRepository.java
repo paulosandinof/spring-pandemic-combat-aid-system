7b63f8f8c0faca7e09e1b190110e7ba6bc5bf014
Paulo Sandino <paulosandino@gmail.com>
2021-09-13 23:02:10 -0300

    updated findAll method to return a list instead of a iterable

--- src/main/java/com/sandino/pandemiccombataidsystem/repositories/HospitalRepository.java
+++ src/main/java/com/sandino/pandemiccombataidsystem/repositories/HospitalRepository.java
@@ -1,9 +1,11 @@
 package com.sandino.pandemiccombataidsystem.repositories;
 
+import java.util.List;
+
 import com.sandino.pandemiccombataidsystem.entities.Hospital;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface HospitalRepository extends CrudRepository<Hospital, String> {
-
+    List<Hospital> findAll();
 }
