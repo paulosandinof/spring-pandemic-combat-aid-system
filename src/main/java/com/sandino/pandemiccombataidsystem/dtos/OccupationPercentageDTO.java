09a342c2ae7ee84d360a9c994353c98ad9b3378c
Paulo Sandino <paulosandino@gmail.com>
2021-09-13 22:59:55 -0300

    created the controller method, the DTO class and service methods

---
+++ src/main/java/com/sandino/pandemiccombataidsystem/dtos/OccupationPercentageDTO.java
@@ -0,0 +1,21 @@
+package com.sandino.pandemiccombataidsystem.dtos;
+
+public class OccupationPercentageDTO {
+
+    private double occupationPercentage;
+
+    public OccupationPercentageDTO() {
+    }
+
+    public OccupationPercentageDTO(double occupationPercentage) {
+        this.occupationPercentage = occupationPercentage;
+    }
+
+    public double getOccupationPercentage() {
+        return occupationPercentage;
+    }
+
+    public void setOccupationPercentage(double occupationPercentage) {
+        this.occupationPercentage = occupationPercentage;
+    }
+}
