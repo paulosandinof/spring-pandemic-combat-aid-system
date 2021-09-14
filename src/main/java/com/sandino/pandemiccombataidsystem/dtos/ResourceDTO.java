3a090ce4b07e08ec7045b137b2692efcb48ec8d4
Paulo Sandino <paulosandino@gmail.com>
2021-09-13 21:26:41 -0300

    created dtos

---
+++ src/main/java/com/sandino/pandemiccombataidsystem/dtos/ResourceDTO.java
@@ -0,0 +1,34 @@
+package com.sandino.pandemiccombataidsystem.dtos;
+
+public class ResourceDTO {
+
+    private String name;
+
+    private String type;
+
+    public ResourceDTO(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourceDTO [name=" + name + ", type=" + type + "]";
+    }
+}
