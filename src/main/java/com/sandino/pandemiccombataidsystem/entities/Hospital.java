4a9e8bb8c207c62c071b880e7e3bb03b7a3c98c2
Paulo Sandino <paulosandino@gmail.com>
2021-09-13 23:01:28 -0300

    removed lombok annotations to use getters in code

--- src/main/java/com/sandino/pandemiccombataidsystem/entities/Hospital.java
+++ src/main/java/com/sandino/pandemiccombataidsystem/entities/Hospital.java
@@ -10,17 +10,6 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 @Entity
 public class Hospital {
 
@@ -51,6 +40,9 @@ public class Hospital {
     @OneToMany
     private List<Resource> resources;
 
+    public Hospital() {
+    }
+
     public Hospital(String name, String address, String cnpj, double latitude, double longitude,
             double occupationPercentage, List<Resource> resources) {
         this.name = name;
@@ -60,5 +52,88 @@ public class Hospital {
         this.longitude = longitude;
         this.occupationPercentage = occupationPercentage;
         this.resources = resources;
+    }
+
+    public Hospital(String id, String name, String address, String cnpj, double latitude, double longitude,
+            double occupationPercentage, List<Resource> resources) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cnpj = cnpj;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.occupationPercentage = occupationPercentage;
+        this.resources = resources;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getOccupationPercentage() {
+        return occupationPercentage;
+    }
+
+    public void setOccupationPercentage(double occupationPercentage) {
+        this.occupationPercentage = occupationPercentage;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<Resource> resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital [address=" + address + ", cnpj=" + cnpj + ", id=" + id + ", latitude=" + latitude
+                + ", longitude=" + longitude + ", name=" + name + ", occupationPercentage=" + occupationPercentage
+                + ", resources=" + resources + "]";
     }
 }
