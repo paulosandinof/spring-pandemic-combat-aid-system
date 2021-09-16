2bb94f9100879c738f1623f2bdd7e15addbc4972
Paulo Sandino <paulosandino@gmail.com>
2021-09-16 13:32:59 -0300

    overrided methods in the interface to the used in the service

--- src/main/java/com/sandino/pandemiccombataidsystem/repositories/ResourceRepository.java
+++ src/main/java/com/sandino/pandemiccombataidsystem/repositories/ResourceRepository.java
@@ -1,8 +1,15 @@
 package com.sandino.pandemiccombataidsystem.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.sandino.pandemiccombataidsystem.entities.Hospital;
 import com.sandino.pandemiccombataidsystem.entities.Resource;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ResourceRepository extends CrudRepository<Resource, String> {
+    List<Resource> findAll();
+    List<Resource> findAllById(Iterable<String> ids);
+    Optional<Resource> findByIdAndHospital(String id, Hospital hospital);
 }
